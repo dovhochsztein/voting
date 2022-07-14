54b79d259c8d508d1184bba24212044c0317208d
hochsztein.dov <hochsztein.dov@aurora.aero>
2022-07-14 15:51:22 -0400

    working randomizer and coalition randomizer classes

--- voting.py
+++ voting.py
@@ -2,18 +2,136 @@ import random
 import numpy as np
 import itertools
 import time
+from typing import Union, Callable
+
+# todo for architecture: Class for Axioms - base class has blank checker method,
+#  subclasses have implements checker methods. include flag for check
+#  class for randomizer, subclasses for different ways (subclasses for uniform,
+#  coalition voter, etc)
+#  class for election profile, method to evaluate with different ways
+#  class for results evaluation, subclasses for different ways (fptp, bordas, instant runoffs)
+
 
 class Vote:
+    """
+    Class for a single vote object. Automatically generates a ranking on
+    instantiation based on a generator function.
+    """
     def __init__(self, number_of_alternatives, randomizer_fn):
         ranking = randomizer_fn(number_of_alternatives)
         self.ranking = ranking
+        self.preference_cache = dict()
+
+    def first_choice(self, removed_alternatives: Union[set, None] = None):
+        """
+        Gives the first choice of a voter.
+        :param removed_alternatives:
+        :return:
+        """
+        if removed_alternatives is None:
+            removed_alternatives = set()
+        choice = None
+        for alternative in self.ranking:
+            if alternative not in removed_alternatives:
+                choice = alternative
+                break
+        return choice
+
     def prefers(self, first_alternative, second_alternative):
+        """
+        checks preference between first and second alternatives
+        returns True if first is preferred, False if second, None if no preferrence
+        """
+        if (first_alternative, second_alternative) in self.preference_cache:
+            return self.preference_cache[
+                (first_alternative, second_alternative)]
         ranking = self.ranking
         if first_alternative in ranking and second_alternative in ranking:
-            if ranking.index(first_alternative) < ranking.index(second_alternative):
+            if ranking.index(first_alternative) < \
+                    ranking.index(second_alternative):
+                self.preference_cache[
+                    (first_alternative, second_alternative)] = True
                 return True
-        return False
+        else:
+            if first_alternative in ranking:
+                self.preference_cache[
+                    (first_alternative, second_alternative)] = True
+                return True
+            elif second_alternative in ranking:
+                self.preference_cache[
+                    (first_alternative, second_alternative)] = False
+                return True
+            else:
+                self.preference_cache[
+                    (first_alternative, second_alternative)] = None
+                return None
 
+
+class Randomizer:
+    """
+    Base class for randomizer object to generate a voter ranking. Default
+    generate method assumes a uniform distribution.
+    """
+    def __init__(self, number_of_alternatives: int = 2, number_to_rank_funx: Union[Callable, str, None] = None):
+        self.number_of_alternatives = number_of_alternatives
+        if number_to_rank_funx is None:
+            self.number_to_rank_funx = lambda: self.number_of_alternatives
+        elif type(number_to_rank_funx) is str:
+            if number_to_rank_funx == 'all':
+                self.number_to_rank_funx = lambda: self.number_of_alternatives
+            elif number_to_rank_funx == 'uniform':
+                self.number_to_rank_funx =\
+                    lambda: random.randint(1, self.number_of_alternatives)
+        else:
+            self.number_to_rank_funx = number_to_rank_funx
+
+    def generate_rank(self):
+        number_to_rank = self.number_to_rank_funx()
+        ranking = random.sample(list(range(self.number_of_alternatives)),
+                                number_to_rank)
+        return ranking
+
+A = Randomizer(5, 'uniform')
+
+
+class CoalitionRandomizer(Randomizer):
+    def __init__(self, coalition_sizes: list, number_to_rank_funx: Union[Callable, str, None] = None):
+        number_of_alternatives = sum(coalition_sizes)
+        self.coalition_sizes = coalition_sizes
+        super().__init__(number_of_alternatives, number_to_rank_funx)
+        self.coalition_to_alternative_dict = {}
+        self.alternative_to_coalition_dict = {}
+        index = 0
+        for coalition, size in enumerate(coalition_sizes):
+            self.coalition_to_alternative_dict[coalition] = range(index, index + size)
+            for alternative in range(index, index + size):
+                self.alternative_to_coalition_dict[alternative] = coalition
+            index += size
+
+    def generate_rank(self):
+        start = time.time()
+        number_to_rank = self.number_to_rank_funx()
+        alternatives_to_rank = random.sample(list(range(self.number_of_alternatives)),
+                                number_to_rank)
+        coalitions_to_rank = dict()
+        for alternative in alternatives_to_rank:
+            coalition = self.alternative_to_coalition_dict[alternative]
+            if coalition not in coalitions_to_rank:
+                coalitions_to_rank[coalition] = set()
+            coalitions_to_rank[coalition].add(alternative)
+
+        coalition_order = list(coalitions_to_rank.keys())
+        random.shuffle(coalition_order)
+        ranking = list()
+        for coalition in coalition_order:
+            alternatives = list(coalitions_to_rank[coalition])
+            random.shuffle(alternatives)
+            ranking.extend(alternatives)
+        print(alternatives_to_rank, ranking)
+        print(time.time() - start)
+        return ranking
+
+B = CoalitionRandomizer([4, 3, 2], 'uniform')
 
 def randomizer_1(number_of_alternatives):
     number_to_rank = random.randint(1, number_of_alternatives)
@@ -23,7 +141,11 @@ def randomizer_1(number_of_alternatives):
 
 
 class Election_Profile:
-    def __init__(self, number_of_votes, number_of_alternatives, randomizer_fn):
+    """
+    Class for election profile, all Vote objects are created on instantiation.
+    """
+    def __init__(self, number_of_votes: int, number_of_alternatives: int,
+                 randomizer_fn: Callable):
         self.votes = []
         self.rankings = []
         self.number_of_alternatives = number_of_alternatives
@@ -35,7 +157,7 @@ class Election_Profile:
 
         sub_lists = []
         for ii in range(2, number_of_alternatives):
-            sub_lists.extend(list(itertools.combinations(range(number_of_alternatives), ii)))
+            sub_lists.extend(set(itertools.combinations(range(number_of_alternatives), ii)))
         self.sub_lists = sub_lists
 
     def simplify(self, remaining_alternatives):
@@ -68,12 +190,15 @@ class Axiom:
     def check_axiom(self, election, voting_system):
         pass
 
-def check_iia(voting_system, election):
+def check_iia(voting_system, election, check_all=True):
     number_of_alternatives = election.number_of_alternatives
     sub_lists = election.sub_lists
     society_ranking = voting_system(election.rankings, list(range(number_of_alternatives)))
     for sub_list in sub_lists:
+        if not check_all and society_ranking[0] not in sub_list:
+            continue
         new_society_ranking = voting_system(simplify(election.rankings, sub_list), list(sub_list))
+        print(new_society_ranking)
         if not check_maintains_order(new_society_ranking, society_ranking):
             # print(f'IIA Failed: Ranking with all was {society_ranking}, but the ranking with only the alternatives {sub_list} is {new_society_ranking}')
             return False
@@ -135,7 +260,7 @@ def check_unanimity(voting_system, election):
     return True
 
 now = time.time()
-N = 10000
+N = 100
 iia = np.full(N, False)
 for ii in range(N):
     election = Election_Profile(1000, 5, randomizer_1)
